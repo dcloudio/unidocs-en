bc351ba7d8ddd22e56640333493d767322a7bd7c
qiang <guoshengqiang@live.com>
2021-09-09 17:25:51 +0800

    fix: fix parse.js

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,24 +1,41 @@
-function isSame(origin, target) {
-  const test = /[\s#*\-`|</│└├┌:]*/
-  const originStart = origin.match(test)[0]
-  const targetStart = target.match(test)[0]
-  return (originStart.trimEnd() || originStart) === (targetStart.trimEnd() || targetStart)
+function isSame(origin, target, isCode) {
+  const commentTest1 = /\s*\/\//
+  const commentTest2 = /\s*<!--.+-->/
+  if (isCode) {
+    return (commentTest1.test(origin) || commentTest2.test(origin)) && (commentTest1.test(target) || commentTest2.test(target))
+  }
+  const test1 = /^\s*(\d+\.\s+|```)/
+  const test2 = /^[\s#*\-|<>/│└├┌:]*/
+  const originStart = (origin.match(test1) || origin.match(test2))[0]
+  const targetStart = (target.match(test1) || target.match(test2))[0]
+  return (originStart.trimEnd() || originStart) === (targetStart.trimEnd() || targetStart) && target.replace(test2, '').trim().length
+}
+
+function isPlaceholder(text) {
+  return text && text.includes('...') && /^[\s#*\-|<>/]*$/.test(text)
 }
 
 function parse(str = '') {
   const originTest = /[\u4e00-\u9fa5]/
+  const codeTest = /^\s*(```\s*[a-zA-Z]*)$/
   const origin = []
   const target = []
   const notTranslated = []
   const array = str.split(/\r?\n/)
+  let isCode
   for (let index = 0; index < array.length; index++) {
     const item = array[index]
+    isCode = codeTest.test(item) ? !isCode : isCode
     const nextItem = array[index + 1]
-    if (item && originTest.test(item) && nextItem && !originTest.test(nextItem) && isSame(item, nextItem)) {
+    const isPlaceholderItem = isPlaceholder(nextItem)
+    if (item && originTest.test(item) && nextItem && !originTest.test(nextItem) && isSame(item, nextItem, isCode)) {
       target.push(nextItem)
       index++
     } else {
       target.push(item)
+      if (isPlaceholderItem) {
+        index++
+      }
       if (originTest.test(item)) {
         notTranslated.push(`${`${index + 1}: `.padStart(5) + item}`)
       }
