fbba64fb70a0bd018a6b84255f5259767a298746
DCloud_LXH <283700113@qq.com>
2021-12-23 17:33:09 +0800

    chore: validate use glob

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,20 +1,24 @@
 const fs = require('fs')
 const path = require('path')
+const glob = require('glob')
 const parse = require('./parse')
 
 const argv = process.argv
 const validateResultPath = path.join(__dirname, '..', `validate_result.md`)
 
-function runner(mdPath, clear = true) {
+function runner(mdPath = '', clear = true) {
   if (mdPath.endsWith('.md')) {
     validateMd(path.join(__dirname, '../docs', mdPath.replace(/^docs\//, '')))
   } else {
     clear && (fs.existsSync(validateResultPath) && fs.unlinkSync(validateResultPath))
-    const mdFileDir = path.resolve(__dirname, '../docs', mdPath)
-    fs.readdir(mdFileDir, (err, files) => {
-      files.forEach(fileName => {
-        if (!fileName.startsWith('.'))
-          validateMd(path.join(mdFileDir, fileName), fileName)
+    glob(`docs/${mdPath}/**/*.`, (err, files) => {
+      if(err) {
+        console.error(err);
+        return;
+      }
+      files.forEach(filePath => {
+        const mdPath = path.resolve(__dirname, `../${filePath}`)
+        validateMd(mdPath,path.basename(mdPath))
       })
     })
   }
@@ -23,9 +27,6 @@ function runner(mdPath, clear = true) {
 runner(argv.length > 2 ? argv[argv.length - 1] : '')
 
 function validateMd(mdPath, fileName) {
-  if (!mdPath.endsWith('.md')) {
-    return runner(mdPath, false);
-  }
   fs.readFile(mdPath, { encoding: 'utf-8' }, (err, data) => {
     if (err) {
       throw err
